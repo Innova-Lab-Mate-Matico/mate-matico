6c5ac5e4bfaba39c015c7c0b1515f5602002d746
johnnychurra-arch <johnnychurra@gmail.com>
2026-06-08 19:55:00 -0300

    fix(front): cambiar atributo class por className en Header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,11 +3,11 @@ import React from 'react';
 export default function Header() {
     return (
         <header id="arriba">
-        <img class="logo" src="./img/matemático.png" alt="el matemático" />
+        <img className="logo" src="./img/matemático.png" alt="el matemático" />
         <h1>EL MATEMÁTICO</h1>
         <h3>Matemática para la vida real</h3>
         <br />
         <p>Aprendé con ejemplos simples sobre compras, descuentos, deudas, sueldo y decisiones cotidianas.</p>
         </header>
     );
-}
+}
